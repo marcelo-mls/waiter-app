1bdd2902e3f5556b1eb77ebbaea75d61f0009e1a
Marcelo Marques <marcelo-mls@hotmail.com>
2022-11-15 21:53:05 -0300

    Estiliza o Header

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -1,10 +1,11 @@
 import { GlobalStyles } from './styles/GlobalStyles';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <h1> Waiter App </h1>
+      <Header />
     </>
   );
 }
