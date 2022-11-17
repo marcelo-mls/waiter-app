3dd5f873cb3508f204c6f97da20f29203831ad7e
Marcelo Marques <marcelo-mls@hotmail.com>
2022-11-16 21:46:23 -0300

    Criar e faz configurações iniciais para Mobile

---
+++ app/src/components/Main/index.tsx
@@ -0,0 +1,12 @@
+import { Container } from './styles';
+import Header from '../Header';
+
+function Main() {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+}
+
+export default Main;
