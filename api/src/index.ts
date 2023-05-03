78374b66d9df1f6f03f3d227024c176da55c423f
marcelo marques <marcelo-mls@hotmail.com>
2023-05-03 02:40:13 -0300

    feat: adiciona prints

--- api/src/index.ts
+++ api/src/index.ts
@@ -11,24 +11,26 @@ export const io = new Server(server);
 
 // npm run dev
 // docker run --name mongo -p 27017:27017 -d mongo
-mongoose.connect('mongodb://localhost:27017').then(() => {
-  const PORT = 3001;
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => console.log('MongoDB conectado com sucesso!'))
+  .then(() => {
+    const PORT = 3001;
 
-  app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', '*');
-    res.setHeader('Access-Control-Allow-Headers', '*');
-    next();
-  });
-  app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads'))); //unix
-  // app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads').replace('\\src', ''))); // windows
+    app.use((req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', '*');
+      res.setHeader('Access-Control-Allow-Headers', '*');
+      next();
+    });
+    app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads'))); //unix
+    // app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads').replace('\\src', ''))); // windows
 
-  app.use(express.json());
-  app.use(router);
+    app.use(express.json());
+    app.use(router);
 
-  server.listen(PORT, () => {
-    console.log('=================================================');
-    console.log(`| 🚀 Servidor rodando em: http://localhost:${PORT} |`);
-    console.log('=================================================');
-  });
-}).catch(() => console.log('Erro ao conectar no MongoDB'));
+    server.listen(PORT, () => {
+      console.log('=================================================');
+      console.log(`| 🚀 Servidor rodando em: http://localhost:${PORT} |`);
+      console.log('=================================================');
+    });
+  }).catch(() => console.log('Erro ao conectar no MongoDB'));
